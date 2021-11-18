774d3e9fbb571024f29d5d6499a78f1c30da554a
lvisentin <lvise.batista@gmail.com>
2021-11-18 15:01:27 -0300

    adding loading animation

--- src/app/shared/components/box-list/box-list.component.ts
+++ src/app/shared/components/box-list/box-list.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Apollo, gql } from 'apollo-angular';
+import { Subject } from 'rxjs';
 import { Box, BoxResponse } from '../../models/boxes.model';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'agg-box-list',
   templateUrl: './box-list.component.html',
   styleUrls: ['./box-list.component.scss']
 })
-export class BoxListComponent implements OnInit {
+export class BoxListComponent implements OnInit, OnDestroy {
 
   public boxes: BoxResponse[] = [];
   public isLoading: boolean = false;
 
+  private destroy$: Subject<boolean> = new Subject();
   private GET_BOXES = gql`
     query getboxes {
       boxes(free: false, purchasable: true, openable: true) {
@@ -33,13 +36,21 @@ export class BoxListComponent implements OnInit {
     this.getBoxes();
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.unsubscribe();
+    this.destroy$.next(false);
+  }
+
   private getBoxes(): void {
+    this.isLoading = true;
+
     this.apollo
       .watchQuery({ query: this.GET_BOXES })
       .valueChanges
+      .pipe(takeUntil(this.destroy$))
       .subscribe(({ data }: { data: any }) => {
         this.boxes = data.boxes.edges;
-        // console.log(data.boxes.edges)
+        this.isLoading = false;
       })
   }
 
